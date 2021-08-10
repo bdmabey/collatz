af3559fffb39ff36a4a388dd96fbb9f671ca6cb7
Braden Mabey <braden.mabey@gmail.com>
2021-08-10 12:09:38 -0400

    tried somethig new

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
         linked_branches: vec![]
     };
 
-    test.numbers = collatz_cli::collatz::create_branch_numbers(limit, test.spawn_number as i32);
+    test.numbers = collatz_cli::collatz::Branch::create_branch_numbers(limit, test.spawn_number as i32);
 
     let alternate = collatz_cli::collatz::check_for_branch(test.numbers);
 
